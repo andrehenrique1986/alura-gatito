5137f7abbe45b05d8ee27d9e881c7ee97b304789
andre.silva <andre.silva@marimex.com.br>
2024-05-27 14:53:20 -0300

    Gatito - Primeira Tela

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { SafeAreaView, StatusBar , FlatList } from "react-native";
+import Item from "./Item";
+
+
+
+const Servicos = () => {
+
+    const servicos = [
+        {
+            id: 1,
+            nome: "Banho",
+            preco: 79.90,
+            descricao: "NÃO DÊ BANHO NO SEU GATO ! Mas se precisar nós damos."
+        },
+        {
+            id: 2,
+            nome: "Vacina V4",
+            preco: 89.90,
+            descricao: "Uma dose da vacina V4. Seu gato precisa de duas"
+        },
+        {
+            id: 3,
+            nome: "Vacina Antirrábica",
+            preco: 99.90,
+            descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano"
+        }
+    ]
+
+    return (
+        <SafeAreaView>
+        <StatusBar/>
+             <FlatList
+                data={servicos}
+                renderItem={({item}) => <Item {...item}/>}
+                keyExtractor={({id}) => String(id)}
+             />
+        </SafeAreaView>
+       
+    )
+}
+
+export default Servicos;
