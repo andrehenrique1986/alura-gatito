448ba36ab9aaeef7c2f1c6166914279c0c7d1ec9
andre.silva <andre.silva@marimex.com.br>
2024-05-28 11:52:04 -0300

    Gatito - Reactotron - Projeto Final

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Rotas from './src/Rotas.js';
 import TelaPadrao from './src/componentes/TelaPadrao';
+import reactotron from 'reactotron-react-native';
 
 
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
+reactotron.configure().useReactNative().connect();
+console.tron = reactotron;
 
 const App = () => {
+
+   console.tron.log({
+    curso: "C#"
+  });
+  
   return <TelaPadrao><Rotas /></TelaPadrao>;
 }
 
